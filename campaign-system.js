ae3790de3177c7b378d1aba13734de975f72a657
maxdurbin1-cell <maxdurbin1@gmail.com>
2026-05-26 19:08:19 +0000

    Add inline /prompt helper under campaign chat inputs

--- campaign-system.js
+++ campaign-system.js
@@ -4594,6 +4594,7 @@
       + '<div id="campaignDockTimelineActions" class="campaign-dock-timeline-actions"></div>'
       + '<div class="campaign-dock-chat">'
       + '<input id="campaignDockChatInput" class="campaign-dock-input" type="text" maxlength="500" placeholder="Type campaign chat...">'
+      + '<div class="campaign-dock-chat-helper">Tip: GM can run <strong>/prompt valor d6 @wayfarer breach door</strong> from chat.</div>'
       + '<button class="btn btn-xs btn-teal" onclick="window.campaignSystem.sendChatMessage()">Send</button>'
       + '<button class="btn btn-xs" onclick="window.campaignSystem.clearRecentDockChat()">Clear Recent</button>'
       + '<button class="btn btn-xs" onclick="window.campaignSystem.restoreRecentDockChat()" title="Restore last cleared chat batch">Restore</button>'
